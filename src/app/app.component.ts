634a75277f419be3c8ff535d45c92bbc380f0d7f
luis jimenez <eve_ghetto@hotmail.com>
2023-09-08 19:21:55 -0500

    Create basic calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'Calculator';
+
+  number1: number = 0;
+  number2: number = 0;
+  result: number = 0;
+
+  operation(event:Event):void{
+    if ((<HTMLInputElement>event.target).value == "sum") {
+      this.result = this.number1 + this.number2;
+    }
+    else if((<HTMLInputElement>event.target).value == "rest"){
+      this.result = this.number1 - this.number2;
+    }
+    else if ((<HTMLInputElement>event.target).value == "mult") {
+      this.result = this.number1 * this.number2;
+    }
+    else{
+        if (this.number2 == 0) {
+          this.result = 0;
+        }
+        else{
+          this.result = this.number1 / this.number2;
+        }
+    }
+  }
+
+  sum():void{
+    this.result = this.number1 + this.number2;
+  }
+
+  rest():void{
+    this.result = this.number1 - this.number2;
+  }
+
+  mult():void{
+    this.result = this.number1 * this.number2;
+  }
+
+  div():void{
+    this.result = this.number1 / this.number2;
+  }
+
 }
+
+
